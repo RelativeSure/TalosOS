f6c7960f9590e184047083e9d451ae95f3db50cf
null <139585163+KubeArchitectBot@users.noreply.github.com>
2026-01-13 00:55:21 +0000

    Update Terraform hcloud-talos/talos/hcloud to v2.21.4

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -1,6 +1,6 @@
 module "talos" {
   source                    = "hcloud-talos/talos/hcloud"
-  version                   = "2.21.3"
+  version                   = "2.21.4"
   talos_version             = "1.9.5"
   firewall_use_current_ip   = false
   # firewall_kube_api_source  = concat(var.PRIVATE_HOME_IP, tolist(data.spacelift_ips.ips.ips))
